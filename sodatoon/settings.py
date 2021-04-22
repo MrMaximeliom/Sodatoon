c333c613db53aa5961b5b2cb1181ff09a57eb2f2
moayed <hysoca7@gmail.com>
2021-04-22 14:50:56 +0200

    adding authentication classes

--- sodatoon/settings.py
+++ sodatoon/settings.py
@@ -140,9 +140,7 @@ STATICFILES_DIRS = (
 REST_FRAMEWORK = {
 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-   'DEFAULT_AUTHENTICATION_CLASSES': (
-       'rest_framework.authentication.TokenAuthentication',
-   ),
+
 
 
 }
