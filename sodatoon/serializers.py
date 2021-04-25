cf1b963a33d1feb572222d0735de79e9719f780b
moayed <hysoca7@gmail.com>
2021-04-25 13:12:48 +0200

    fixing register model and rename field is_reader to is_artist

--- sodatoon/serializers.py
+++ sodatoon/serializers.py
@@ -225,13 +225,13 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True,label='Confirm Password')
-    is_reader = serializers.ChoiceField(choices=User_TYPES,required=True,label='What are you?')
+    is_artist = serializers.ChoiceField(choices=User_TYPES,required=True,label='What are you?')
     gender = serializers.ChoiceField(choices=GENDER,allow_blank=True)
     country = serializers.ChoiceField(choices=COUNTRIES,allow_blank=True)
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name','gender','country','is_reader')
+        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name','gender','country','is_artist')
         extra_kwargs = {
             'first_name': {'required': True},
             'last_name': {'required': True},
